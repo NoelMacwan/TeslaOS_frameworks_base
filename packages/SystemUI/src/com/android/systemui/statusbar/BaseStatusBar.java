7109a31b34d8a89ff9caf1a372997ceea91309aa
Altaf-Mahdi <altaf.mahdi@gmail.com>
2014-12-27 16:37:13 +0000

    Fix settings not being applied after reboot
    - Blacklist and dnd setting were not being applied at boot
    - Clean up the SettingsObserver
    
    Change-Id: I86080f1b00be7b0962ebd6383faed2ef27deda93

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -305,8 +305,8 @@ public abstract class BaseStatusBar extends SystemUI implements
         }
     };
 
-    class SettingsObserver extends ContentObserver {
-        SettingsObserver(Handler handler) {
+    private class SettingsObserver extends ContentObserver {
+        public SettingsObserver(Handler handler) {
             super(handler);
         }
 
@@ -336,8 +336,6 @@ public abstract class BaseStatusBar extends SystemUI implements
             splitAndAddToArrayList(mBlacklist, blackString, "\\|");
         }
     };
-
-    private SettingsObserver mHeadsUpSettingsObserver = new SettingsObserver(mHandler);
 
     private RemoteViews.OnClickHandler mOnClickHandler = new RemoteViews.OnClickHandler() {
         @Override
@@ -577,11 +575,11 @@ public abstract class BaseStatusBar extends SystemUI implements
                 ServiceManager.checkService(DreamService.DREAM_SERVICE));
         mPowerManager = (PowerManager) mContext.getSystemService(Context.POWER_SERVICE);
 
-        SettingsObserver observer = new SettingsObserver(mHandler);
-        observer.observe();
-
         mDndList = new ArrayList<String>();
         mBlacklist = new ArrayList<String>();
+
+        SettingsObserver observer = new SettingsObserver(mHandler);
+        observer.observe();
 
         mSettingsObserver.onChange(false); // set up
         mContext.getContentResolver().registerContentObserver(
